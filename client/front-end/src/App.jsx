a1fa8312fb3cbce5904874812226da8b59e222cb
Rudra Gupta <guptarudra901@gmail.com>
2025-02-05 15:36:03 +0530

    Completed

--- client/front-end/src/App.jsx
+++ client/front-end/src/App.jsx
@@ -6,7 +6,20 @@ function App() {
 	const [des, setDes] = useState("");
 	const [notes, setNotes] = useState(data);
 	const [count, setCount] = useState(4);
+  const [isRecording, setisRecording] = useState(false);
+  const [note, setNote] = useState(null);
+  const [notesStore, setnotesStore] = useState([]);
+  const SpeechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition;
+const microphone = new SpeechRecognition();
 
+microphone.continuous = true;
+microphone.interimResults = true;
+microphone.lang = "en-US";
+const storeNote = () => {
+setnotesStore([...notesStore, note]);
+setNote("");
+};
 	function remove(id) {
 		setNotes(notes.filter((e) => e.key !== id));
 	}
@@ -22,7 +35,38 @@ function App() {
 		setDes("");
 		console.log(notes);
 	}
-
+  const startRecordController = () => {
+    if (isRecording) {
+      microphone.start();
+      microphone.onend = () => {
+        console.log("continue..");
+        microphone.start();
+      };
+    } else {
+      microphone.stop();
+      microphone.onend = () => {
+        console.log("Stopped microphone on Click");
+      };
+    }
+    microphone.onstart = () => {
+      console.log("microphones on");
+    };
+  
+    microphone.onresult = (event) => {
+      const recordingResult = Array.from(event.results)
+        .map((result) => result[0])
+        .map((result) => result.transcript)
+        .join("");
+      console.log(recordingResult);
+      setNote(recordingResult);
+      microphone.onerror = (event) => {
+        console.log(event.error);
+      };
+    };
+  };
+  useEffect(() => {
+    startRecordController();
+  }, [isRecording]);
 	return (
     <>
     		<div className="App">
@@ -78,20 +122,25 @@ function App() {
 			</div>
 		</div>
     <h1>Record Voice Notes</h1>
-    <div>
-      <div className="noteContainer">
-        <h2>Record Note Here</h2>
-        <button className="button">
-          Save
-        </button>
-        <button >
-          Start/Stop
-        </button>
+      <div>
+        <div className="noteContainer">
+          <h2>Record Note Here</h2>
+          {isRecording ? <span>Recording... </span> : <span>Stopped </span>}
+          <button className="button" onClick={storeNote} disabled={!note}>
+            Save
+          </button>
+          <button onClick={() => setisRecording((prevState) => !prevState)}>
+            Start/Stop
+          </button>
+          <p>{note}</p>
+        </div>
+        <div className="noteContainer">
+          <h2>Notes Store</h2>
+          **{notesStore.map((note) => (
+            <p key={note}>{note}</p>
+          ))}**
+        </div>
       </div>
-      <div className="noteContainer">
-        <h2>Notes Store</h2>
-      </div>
-    </div>
     </>
 	);
 }
